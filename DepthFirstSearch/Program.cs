a3af5846f153b7c2fba0e4971619b6115b6e2637
Zana-Jashanica <zana.jashanica@vizah.ch>
2020-03-09 16:04:25 +0100

    Add project files.

---
+++ DepthFirstSearch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DepthFirstSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BDFS Queen = new BDFS();
+            Queen.solveNQ();
+
+        }
+    }
+}
